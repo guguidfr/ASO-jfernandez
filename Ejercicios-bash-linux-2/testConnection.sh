d1cd8e88f9fc5f8d4c39c76542926cf21404728c
guguidfr <guguidfr@protonmail.com>
2022-09-30 22:03:54 +0200

    testConnection.sh terminado

---
+++ Ejercicios-bash-linux-2/testConnection.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# José Daniel Fernández López
+# 30/09/2022
+# Comprobar estado de una máquina mediante un ping
+
+ping -c 4 $1 > /dev/null 2>&1
+check=$?
+if [ $check -eq 0 ]
+then
+    echo "La máquina con la ip $1 responde. ALIVE MACHINE"
+else 
+    echo "La máquina con la ip $1 no responde. DEAD MACHINE"
+fi
